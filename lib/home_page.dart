d348e299b12030f756261973530896828e3a1ee6
rafabm90 <rafaelborges@arpiatecnologia.com>
2021-08-16 15:44:35 -0300

    Add snackbar and functions

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,6 +15,8 @@ class _HomePageState extends State<HomePage> {
   final _toDoController = TextEditingController();
 
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved = Map();
+  int _lastRemovedPos = 0;
 
   @override
   void initState() {
@@ -120,6 +122,30 @@ class _HomePageState extends State<HomePage> {
           });
         },
       ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved['title']}\" removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              },
+            ),
+            duration: Duration(seconds: 2),
+          );
+
+          ScaffoldMessenger.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
